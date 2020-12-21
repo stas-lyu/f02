76920ac41d19c0ea5a08dfa9fee672a06f86089c
stas-lyu <staslyu1997@gmail.com>
2020-12-21 19:26:39 +0200

    develop

--- src/js/burger.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector(".trigger").onclick = function () {
-//   OpenBurger();
-// };
-document.querySelector(".trigger").addEventListener("click", function Open() {
-  document.getElementById("menu").classList.toggle("show");
-});
